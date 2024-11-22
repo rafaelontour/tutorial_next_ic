a97413d2b7a50f08fbe877ab96d075e7d0498a23
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2024-11-22 00:41:12 -0300

    Primeiro commit

---
+++ src/app/(paginas)/artigos/page.tsx
@@ -0,0 +1,7 @@
+export default function Artigos() {
+    return (
+        <div>
+            <h1>Artigos</h1>
+        </div >
+    )
+}
---
+++ src/app/(paginas)/pesquisadores/page.tsx
@@ -0,0 +1,7 @@
+export default function Pesquisadores() {
+    return (
+        <div>
+            <h1>Pesquisadores</h1>
+        </div>
+    );
+}
