beb988eed5121cf5f6a696e053eda15bb1c5b731
rafaelnargolo <124110743+rafaelnargolo@users.noreply.github.com>
2025-03-10 15:46:06 -0300

    Atualizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h1 className="text-3xl">Aplicação básica Next para tutorial web</h1>
         <p>Clique nos items do menu para ver os exemplos</p>
         <p>Esta é uma aplicação simples onde temos somente as requisições sendo feitas para o backend e exibindo os resultados na tela. O principal é que você olhe como está a estrutura de diretórios e arquivos.</p>
-        <p>Todos os arquivos que tem o código responsável pelas requisições estão na pasta <code className="bg-white px-2 py-1 rounded-md">services</code> e organizados de acordo com suas responsabilidades. Por exemplo, as chamadas para os pesquisadores está no arquivo <code className="bg-white px-2 py-1 rounded-md">pesquisadores.ts</code> e as chamadas para os artigos no arquivo <code className="bg-white px-2 py-1 rounded-md">artigos.ts</code>.</p>
+        <p>Todos os arquivos que tem o código responsável pelas requisições estão na pasta <code className="bg-zinc-500 px-2 py-1 rounded-md">services</code> e organizados de acordo com suas responsabilidades. Por exemplo, as chamadas para os pesquisadores está no arquivo <code className="bg-zinc-500 px-2 py-1 rounded-md">pesquisadores.ts</code> e as chamadas para os artigos no arquivo <code className="bg-zinc-500 px-2 py-1 rounded-md">artigos.ts</code>.</p>
       </div>
 
       <div className="flex flex-col gap-3 mt-5 bg-green-400 rounded-md p-3">
@@ -36,7 +36,7 @@ export default function Home() {
 
         <p>A modelagem para o pesquisador deveria ser algo como:</p>
 
-        <pre className="bg-white p-2 rounded-md">{codigoPesquisador}</pre>
+        <pre className="bg-black p-2 rounded-md">{codigoPesquisador}</pre>
 
         <p>Mas isso é apenas um recorte da modelagem, pois o backend retorna mais informações. Podemos fazer a modelagem com todos os dados retornados do backend, porém não necessariamente precisaremos de todos, dependendo da aplicação. Para este tutorial, a modelagem foi reduzida.</p>
       </div>
